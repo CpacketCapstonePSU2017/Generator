de6cce732271d37c3362ae8f4f9d96d4ff22e6ae
Adrian Cardoza <acardoza@pdx.edu>
2018-02-02 19:08:13 -0800

    added functions for traffic low/high/normal to class

--- Generator-Framework/seasonal_traffic_generator.py
+++ Generator-Framework/seasonal_traffic_generator.py
@@ -1,18 +1,17 @@
 '''
-
+    Author: Adrian Cardoza
 '''
 
 
+import numpy as np
 import sys
 from os import path
 from root import ROOT_DIR
 sys.path.append(path.join(ROOT_DIR, 'CPacket-Common-Modules'))
-from .dist_functions.stats.py import *
-from 
 
 
 class SeasonalTrafficGenerator(object):
-    def __init__(self, size, maximum=None, minimum=0, shape=None, function_type=None):
+    def __init__(self, size, maximum=0, minimum=0, shape=None, distribution_name=None):
         '''
 
         '''
@@ -20,16 +19,96 @@ class SeasonalTrafficGenerator(object):
         self.maximum = maximum
         self.minimum = minimum
         self.shape = shape
-        self.function_type = function_type
+        self.distribution_name = distribution_name
+        self.distribution_array = None
 
-    def generate_traffic(self, speed):
-        if speed == 0:
-            # generate low traffic
-            print("Fase speed set\n")
-        elif speed == 1:
-            # generate low traffic
-            print("Low speed set\n")
+    def generate_traffic(self):
+        if self.distribution_name == "poisson":
+            # Code to generate specified array
+            # Replace this with code/class that handles distribution functions?
+            lambda_value = self.maximum - (self.maximum / 4)
+            self.distribution_array = np.random.poisson(lam=lambda_value, size=self.size)
+            return self.distribution_array
+        elif self.distribution_name == "beta":
+            # Code to generate specified array
+            # Replace this with code/class that handles distribution functions?
+            print("Functionality not implemented yet!")
+        elif self.distribution_name == "weilbull":
+            # Code to generate specified array
+            # Replace this with code/class that handles distribution functions?
+            print("Functionality not implemented yet!")
         else:
-            # error bad speed
-            print("Error - bad traffic speed\n")
+            # Error bad function
+            print("Error - bad distribution function")
 
+    def generate_low_traffic(self):
+        if self.distribution_name == "poisson":
+            # Code to generate specified array
+            # Replace this with code/class that handles distribution functions?
+            lambda_value = self.maximum - (self.maximum / 4)
+            self.distribution_array = np.random.poisson(lam=lambda_value, size=self.size)
+            return self.distribution_array
+        elif self.distribution_name == "beta":
+            # Code to generate specified array
+            # Replace this with code/class that handles distribution functions?
+            print("Functionality not implemented yet!")
+        elif self.distribution_name == "weilbull":
+            # Code to generate specified array
+            # Replace this with code/class that handles distribution functions?
+            print("Functionality not implemented yet!")
+        else:
+            # Error bad function
+            print("Error - bad distribution function")
+
+    def generate_high_traffic(self):
+        if self.distribution_name == "poisson":
+            # Code to generate specified array
+            # Replace this with code/class that handles distribution functions?
+            lambda_value = self.maximum - (self.maximum / 4)
+            self.distribution_array = np.random.poisson(lam=lambda_value, size=self.size)
+            return self.distribution_array
+        elif self.distribution_name == "beta":
+            # Code to generate specified array
+            # Replace this with code/class that handles distribution functions?
+            print("Functionality not implemented yet!")
+        elif self.distribution_name == "weilbull":
+            # Code to generate specified array
+            # Replace this with code/class that handles distribution functions?
+            print("Functionality not implemented yet!")
+        else:
+            # Error bad function
+            print("Error - bad distribution function")
+
+    def show_array(self):
+        print(self.distribution_array)
+
+
+# Test code Poisson
+test_seasonal_generator = SeasonalTrafficGenerator(10, 100, 0, None, "poisson")
+
+print("Test: Normal")
+test_seasonal_generator.generate_traffic()
+test_seasonal_generator.show_array()
+
+print("Test: Low")
+test_seasonal_generator.generate_low_traffic()
+test_seasonal_generator.show_array()
+
+print("Test: High")
+test_seasonal_generator.generate_high_traffic()
+test_seasonal_generator.show_array()
+
+# Test code Beta
+test_seasonal_generator = SeasonalTrafficGenerator(10, 100, 0, None, "beta")
+
+print("Test: Normal")
+test_seasonal_generator.generate_traffic()
+test_seasonal_generator.show_array()
+
+print("Test: Low")
+test_seasonal_generator.generate_low_traffic()
+test_seasonal_generator.show_array()
+
+print("Test: High")
+test_seasonal_generator.generate_high_traffic()
+test_seasonal_generator.show_array()
