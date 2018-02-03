e4f38ada5065f3a268980caf8d440029802943a4
Hayley Howard <hahoward@pdx.edu>
2018-02-03 14:49:58 -0800

    outline of the new generator function that takes seasonality into account.

--- Generator-Framework/generator.py
+++ Generator-Framework/generator.py
@@ -4,31 +4,38 @@ from os import path
 from root import ROOT_DIR
 sys.path.append(path.join(ROOT_DIR, 'CPacket-Common-Modules'))
 
-class Poisson:
-
-    def __init__(self, size, maximum, minimum=0, shape=None):
-        if maximum is None:
-            raise ValueError("maximum cannot not be None")
-        self._Size = size
-        self._Max = maximum
-        self._Min = minimum
-        self._Shape = shape
-
-        self.Dist_Array = None
-
-    def generate(self):
-        lambda_value = self._Max - (self._Max / 4)
-
-        self.Dist_Array = np.random.poisson(lam=lambda_value, size=self._Size)
-        for x in self.Dist_Array:
-            if x > self._Max:
-                x = self._Max
-        return self.Dist_Array
-
-
-
-
 class Generator:
+    def __init__(self, days, functype, high_max, high_min, low_max, low_min):
+        self._Days = days
+        self._Function = functype
+        self._Increments = 96 * days
+        self._High_Max = high_max
+        self._High_Min = high_min
+        self._Low_Max = low_max
+        self._High_Min = low_min
+
+        remainder = days % 7
+        days_to_convert = days - remainder
+        self._Weeks = days_to_convert / 7
+        self.Dist_Array = None
+        for week in self._Weeks:
+            array = []
+            count = 0
+            while count < 7:
+                if count < 5:
+                    #generate high traffic morning
+                    #generate high traffic work time
+                    #generate high traffic evening
+                    array = np.concatenate([morning, worktime, evening])
+                else:
+                    #generate low traffic morning
+                    #generate low traffic work time
+                    #generate low traffic evening
+                    array = np.concatenate([morning, worktime, evening])
+                self.Dist_Array = np.concat([self.Dist_Array, array])
+
+
+'''class Generator:
 
 # The way this is set up, every distribution function to be passed in here needs a method called "generate" to do its work.
 
@@ -49,9 +56,4 @@ class Generator:
 
     def show_array(self):
         for x in self.Dist_Array:
-            print(x)
-
-
-poisson_test = Poisson(100, 1.25 * 10000000)
-generator_test = Generator(poisson_test)
-generator_test.show_array()
+            print(x)'''
