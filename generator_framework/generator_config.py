51416b49afa05f44e0276475760d2e6367bfdef4
Adrian Cardoza <acardoza@pdx.edu>
2018-02-05 21:23:44 -0800

    Updating generator_config.py variable names to match Generator and Stats formatting

--- generator_framework/generator_config.py
+++ generator_framework/generator_config.py
@@ -9,31 +9,24 @@ class GeneratorConfig:
         '''
 
         '''
-        self.time = 7
-        self.high_day_max = 100
-        self.high_day_min = 0
-        self.low_day_max = 50
-        self.low_day_min = 0
-        self.business_hours = 6
-        self.distribution_function = "poisson"
+        self._Days = 7
+        self._High_Max = 100
+        self._High_Min = 0
+        self._Low_Max = 50
+        self._Low_Min = 0
+        self._Business_Hours = 6
+        self._Func_Type = "poisson"
 
-    def set_config(self, time=None, high_day_max=None, high_day_min=None, low_day_max=None, low_day_min=None,
-                   business_hours=None, distribution_function=None):
-        self.time = time
-        self.high_day_max = high_day_max
-        self.high_day_min = high_day_min
-        self.low_day_max = low_day_max
-        self.low_day_min = low_day_min
-        self.business_hours = business_hours
-        self.distribution_function = distribution_function
+    def set_config(self, days=None, high_day_max=None, high_day_min=None, low_day_max=None, low_day_min=None,
+                   business_hours=None, funct_type=None):
+        self._Days = days
+        self._High_Max = high_day_max
+        self._High_Min = high_day_min
+        self._Low_Max = low_day_max
+        self._Low_Min = low_day_min
+        self._Business_Hours = business_hours
+        self._Func_Type = funct_type
 
     def get_config(self):
-        print("configuration: %d %d %d %d %d %d %s" % (self.time, self.high_day_max, self.high_day_min,
-              self.low_day_max, self.low_day_min, self.business_hours, self.distribution_function))
-
-
-#Test Code
-test_generator_config = GeneratorConfig()
-test_generator_config.get_config()
-test_generator_config.set_config(14, 200, 0, 100, 0, 12, "beta")
-test_generator_config.get_config()
+        print("configuration: %d %d %d %d %d %d %s" % (self._Days, self._High_Max, self._High_Min,
+              self._Low_Max, self._Low_Min, self._Business_Hours, self._Func_Type))
