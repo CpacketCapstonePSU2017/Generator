e6ca53b46b7bfc22869ca2f088cdb2c543ba8227
Adrian Cardoza <acardoza@pdx.edu>
2018-02-05 21:27:05 -0800

    Fixing generator_config.py variable names...

--- generator_framework/generator_config.py
+++ generator_framework/generator_config.py
@@ -9,24 +9,24 @@ class GeneratorConfig:
         '''
 
         '''
-        self._Days = 7
-        self._High_Max = 100
-        self._High_Min = 0
-        self._Low_Max = 50
-        self._Low_Min = 0
-        self._Business_Hours = 6
-        self._Func_Type = "poisson"
+        self.Days = 7
+        self.High_Max = 100
+        self.High_Min = 0
+        self.Low_Max = 50
+        self.Low_Min = 0
+        self.Business_Hours = 6
+        self.Func_Type = "poisson"
 
     def set_config(self, days=None, high_day_max=None, high_day_min=None, low_day_max=None, low_day_min=None,
                    business_hours=None, funct_type=None):
-        self._Days = days
-        self._High_Max = high_day_max
-        self._High_Min = high_day_min
-        self._Low_Max = low_day_max
-        self._Low_Min = low_day_min
-        self._Business_Hours = business_hours
-        self._Func_Type = funct_type
+        self.Days = days
+        self.High_Max = high_day_max
+        self.High_Min = high_day_min
+        self.Low_Max = low_day_max
+        self.Low_Min = low_day_min
+        self.Business_Hours = business_hours
+        self.Func_Type = funct_type
 
     def get_config(self):
-        print("configuration: %d %d %d %d %d %d %s" % (self._Days, self._High_Max, self._High_Min,
-              self._Low_Max, self._Low_Min, self._Business_Hours, self._Func_Type))
+        print("configuration: %d %d %d %d %d %d %s" % (self.Days, self.High_Max, self.High_Min,
+              self.Low_Max, self.Low_Min, self.Business_Hours, self.Func_Type))
