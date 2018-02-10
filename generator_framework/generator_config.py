5bf68f24df251b8c7e70f7a11245f8495fd00eae
Adrian Cardoza <acardoza@pdx.edu>
2018-02-09 18:59:33 -0800

    Adding Shape variable to GeneratorConfig() Class

--- generator_framework/generator_config.py
+++ generator_framework/generator_config.py
@@ -18,10 +18,11 @@ class GeneratorConfig:
         self.Low_Min = 0
         self.Business_Hours = 6
         self.Func_Type = "poisson"
+        self.Shape = 0
         self.Start_Date = datetime.date.today()
 
     def set_config(self, days=None, high_day_max=None, high_day_min=None, low_day_max=None, low_day_min=None,
-                   business_hours=None, funct_type=None, start_date=None):
+                   business_hours=None, funct_type=None, shape=None, start_date=None):
         self.Days = days
         self.High_Max = high_day_max
         self.High_Min = high_day_min
@@ -29,8 +30,16 @@ class GeneratorConfig:
         self.Low_Min = low_day_min
         self.Business_Hours = business_hours
         self.Func_Type = funct_type
+        self.Shape = shape
         self.Start_Date = start_date
 
     def get_config(self):
-        print("configuration: %d %d %d %d %d %d %s %s" % (self.Days, self.High_Max, self.High_Min,
-              self.Low_Max, self.Low_Min, self.Business_Hours, self.Func_Type, self.Start_Date))
+        print("configuration: %d %d %d %d %d %d %s %d %s" % (self.Days, self.High_Max, self.High_Min,
+              self.Low_Max, self.Low_Min, self.Business_Hours, self.Func_Type, self.Shape, self.Start_Date))
+
+
+#Test Code
+test_generator_config = GeneratorConfig()
+test_generator_config.get_config()
+test_generator_config.set_config(14, 200, 0, 100, 0, 12, "weibull", 1)
+test_generator_config.get_config()
