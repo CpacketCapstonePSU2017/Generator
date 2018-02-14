f4f8cb8e323c559337334f7c6311e2478eff9b77
Hayley Howard <hahoward@pdx.edu>
2018-02-14 14:24:05 -0800

    fixed error with generating the correct number of days' worth of data.

--- resources/weibull.py
+++ resources/weibull.py
@@ -28,26 +28,28 @@ class Weibull:
     def generate(self):
         count = self._Config.Start_Date.weekday()
         iter = 0
+        days = 0
         iterArray = []
         while iter < self._Weeks:
             iterArray.append(iter)
             iter += 1
         for week in iterArray:
             array = np.array([])
-            while count < 7:
-                if count < 5:
-                    #weekday - high traffic during work hours
-                    array = np.concatenate([self.get_array(self._Config.Low_Max, self.morning_hours), self.get_array(self._Config.High_Max, self.work_hours), self.get_array(self._Config.Low_Max, self.evening_hours)])
-                else:
-                    #weekend - low traffic
-                    array = self.get_array(self._Config.Low_Max, self.Increments_Per_Day)
-                if self.Dist_Array is None:
-                    self.Dist_Array = array
-                else:
-                    self.Dist_Array = np.concatenate([self.Dist_Array, array])
-                count += 1
-            count = 0
+            while days < self._Config.Days:
+                while count < 7 and days < self._Config.Days:
+                    if count < 5:
+                        #weekday - high traffic during work hours
+                        array = np.concatenate([self.get_array(self._Config.Low_Max, self.morning_hours), self.get_array(self._Config.High_Max, self.work_hours), self.get_array(self._Config.Low_Max, self.evening_hours)])
+                    else:
+                        #weekend - low traffic
+                        array = self.get_array(self._Config.Low_Max, self.Increments_Per_Day)
+                    if self.Dist_Array is None:
+                        self.Dist_Array = array
+                    else:
+                        self.Dist_Array = np.concatenate([self.Dist_Array, array])
+                    count += 1
+                    days += 1
+                count = 0
         start_date = str(self._Config.Start_Date) + "T00:00:00.00"
         result_datetimes = pd.date_range(start_date, periods=self._Increments, freq='15min')
         return np.array([result_datetimes, self.Dist_Array]).transpose()
-
