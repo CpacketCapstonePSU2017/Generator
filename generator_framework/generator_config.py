2bbf8035034ad36bc7e55e78996d214c0b94881b
Adrian Cardoza <acardoza@pdx.edu>
2018-03-14 20:34:07 -0700

    Adding comment code to GeneratorConfig class

--- generator_framework/generator_config.py
+++ generator_framework/generator_config.py
@@ -10,6 +10,10 @@ from resources.poisson import *
 class GeneratorConfig:
 
     def __init__(self):
+        '''
+        Initialization for the GeneratorConfig class. Holds the default values that will be used
+        to configure the Generator. Update them to desired values.
+        '''
         self.Database = 'generated_data'
         self.Days = 7
         self.High_Max = 1.25 * 10000000
@@ -26,6 +30,23 @@ class GeneratorConfig:
     def set_config(self, database='generated_data', days=7, high_max=1.25 * 10000000, high_min=0,
                    low_max=1.25 * 1500000, low_min=0, business_hours=6, shape=.5, start_date=datetime.date.today(),
                    work_hour_start=8, scale=1000000, func_type='poisson'):
+        '''
+        Update every member variable of the GeneratorConfig class
+
+        :param database: name of the database
+        :param days: number of days to generate data
+        :param high_max: high maximum value
+        :param high_min: high minimum value
+        :param low_max: low maximum value
+        :param low_min: low minimum value
+        :param business_hours: business hours
+        :param shape: shape value
+        :param start_date: start date
+        :param work_hour_start: work hours start value
+        :param scale: scale value
+        :param func_type: function type, either "poisson" or "weibull"
+        :return: none
+        '''
         self.Database = database
         self.Days = days
         self.High_Max = high_max
@@ -45,6 +66,12 @@ class GeneratorConfig:
             self.Func_Type = None
 
     def set_func_type(self, func_type):
+        '''
+        Update the distribution function type for the GeneratorConfig class
+
+        :param func_type: function type, either "poisson" or "weibull"
+        :return: none
+        '''
         if func_type == 'poisson':
             self.Func_Type = Poisson(self)
         elif func_type == 'weibull':
@@ -53,6 +80,11 @@ class GeneratorConfig:
             self.Func_Type = None
 
     def get_config(self):
+        '''
+        Get the current configuration for the GeneratorConfig class
+
+        :return: config_data array with GeneratorConfig member variable values
+        '''
         config_data = [self.Database, self.Days, self.High_Max, self.High_Min, self.Low_Max, self.Low_Min,
                        self.Business_Hours, self.Shape, self.Start_Date, self.Work_Hour_Start, self.Scale,
                        self.Func_Type.Name]
